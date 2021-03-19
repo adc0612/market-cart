9ebc02e78f155e02f846c128206d5b604c741694
Dohu <adc0612@gmail.com>
2021-03-19 22:29:03 +0900

    increment, decrement, delete function added with props and event

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-    state = {
-        count: 0,
-    };
     handleIncrement = () => {
-        // state 오브젝트 안에있는 count를 증가 한 뒤 state를 업데이트 해야 함
-        // state를 업데이트 했을 때는 바로 state에 값을 넣거나 연산을 하면 안됨
-        // this.state.count += 1;
-        // this.setState를 이용해서 꼭 state를 변경해야한다.
-        this.setState({count: this.state.count + 1});
+        this.props.onIncrement(this.props.product);
     };
     handleDecrement = () => {
-        const count = this.state.count - 1;
-        // count에 state의 count값을 집어넣고 만약 0이면 더이상 감소되지않고 0으로 셋팅하고 아니면 감소시켜준다.
-        this.setState({count: count < 0 ? 0 : count});
-    }
+        this.props.onDecrement(this.props.product);
+        
+    };
+    handleDelete = () => {
+        this.props.onDelete(this.props.product);
+    };
     render() {
         const {name, count} = this.props.product;
         return (
